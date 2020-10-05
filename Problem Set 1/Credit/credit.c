6ea97b43dd4e03e11ed72c707ea6331eded3680a
Giovanni Moratto <62956220+GiovanniMoratto@users.noreply.github.com>
2020-10-05 11:07:57 -0300

    Update credit.c

--- Problem Set 1/Credit/credit.c
+++ Problem Set 1/Credit/credit.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <cs50.h>
 
 int main(void)
 {
-    long creditCardNumber;
+    long creditCardNumber; // Declare variable
 
     do
     {
-        printf("Number: \n"); //user input
-        scanf("%ld", &creditCardNumber);
+        creditCardNumber = get_long("Number: \n"); //take user input
     }
     while (creditCardNumber <= 0);
 
-    long workingCC = creditCardNumber;
-    int sum = 0;
-    int count = 0;
-    long divisor = 10;
-    char result[11];
+    long workingCC = creditCardNumber; // Declare variable
+    int sum = 0; // Declare variable
+    int count = 0; // Declare variable
+    long divisor = 10; // Declare variable
+    char result[11]; // Declare variable
 
     // 1st case
-    while (workingCC > 0)
+    while (workingCC > 0) // while loop
     {
-        int lastDigit = workingCC % 10;
-        sum = sum + lastDigit;
-        workingCC = workingCC / 100;
+        int lastDigit = workingCC % 10; // get 1 digit to analyse
+        sum = sum + lastDigit; // sum the digits
+        workingCC = workingCC / 100; // take every two digits
     }
 
     // 2nd case
-    workingCC = creditCardNumber / 10;
-    while (workingCC > 0)
+    workingCC = creditCardNumber / 10; // skip the last digit
+
+    while (workingCC > 0) // while loop
     {
-        int lastDigit = workingCC % 10;
-        int timesTwo = lastDigit * 2;
-        
-        sum = sum + (timesTwo % 10) + (timesTwo / 10);
-        workingCC = workingCC / 100;
+        int lastDigit = workingCC % 10; // get 1 digit to analyse
+        int timesTwo = lastDigit * 2; // multiply by 2
+
+        sum = sum + (timesTwo % 10) + (timesTwo / 10); // sum the digits add those products’ digits (i.e., not the products themselves)
+        workingCC = workingCC / 100; // take every two digits
     }
 
     // length of the number / digit count
@@ -51,8 +52,8 @@ int main(void)
         divisor = divisor * 10;
     }
 
-    int firstDigit = creditCardNumber / divisor;
-    int firstTwoDigits = creditCardNumber / (divisor / 10);
+    int firstDigit = creditCardNumber / divisor; // Declare variable
+    int firstTwoDigits = creditCardNumber / (divisor / 10); // Declare variable
 
     // final check brand card
     if (sum % 10 == 0)
@@ -69,7 +70,7 @@ int main(void)
         {
             strcpy(result, "MASTERCARD");
         }
-        else 
+        else
         {
             strcpy(result, "INVALID"); //INVALID
         }
